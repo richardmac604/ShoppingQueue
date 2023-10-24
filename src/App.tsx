b122c4b8347bf9ba4e9da09671beda45a896ce63
Richard Mac <78776822+richardmac604@users.noreply.github.com>
2023-10-24 16:25:15 -0700

    shopping queue implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [itemsinPersonCart ,setItemsInPersonCart] = useState(0)
+  const [lines, setLines] = useState([[10,5,2],[1],[2],[3],[4]])
+ 
+  function addPersonToLine(e: React.FormEvent<HTMLFormElement>){
+    e.preventDefault();
+
+    //loop through all lines 
+    //find the lines with the Least items 
+    //push the itemsinPersonCart to the line
+
+    let leastItemAmount = 1e9
+    let lineWithLeast: number[] | undefined = undefined;
+
+    for(let line of lines){
+      const totalInLine = line.reduce((sum, value) => sum + value, 0);
+      console.log(totalInLine)
+      if(totalInLine < leastItemAmount){
+        leastItemAmount = totalInLine
+        lineWithLeast = line
+      }
+    }
+
+    if(!lineWithLeast) return;
+
+    setLines(prevLines => prevLines.map((line) => {
+      if (line === lineWithLeast){
+        return[...line, itemsinPersonCart]
+      }else{
+        return line;
+      }
+    }))
+
+  }
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLines(prevLines => {
+        //TODO reduce first item by one in each line
+        return prevLines.map(line => {
+          return[line[0] - 1, ...line.slice(1)]. filter((value)=> value > 0)
+        })
+      })
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    };
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={addPersonToLine}>
+        <input 
+        required
+        type="number"
+        value={itemsinPersonCart}
+        onChange={(e) => setItemsInPersonCart(e.currentTarget.valueAsNumber)}
+        />
+        <button>Checkout</button>
+      </form>
+      
+    <div className='lines'>
+      {lines.map((people,idx) => (
+        <div className = "line" key={idx}>
+
+        {people.map(numberOfItems => 
+        <div>{numberOfItems}</div>) 
+        
+        }
+
+        </div>
+      ))}
     </div>
+  </div>
   );
 }
 
